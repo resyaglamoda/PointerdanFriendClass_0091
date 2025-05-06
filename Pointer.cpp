960f916c7de2b1c30a3b24de86515765e1d1a274
resyaglamoda <resya glamoda suhendri>
2025-05-06 14:57:48 +0700

    Membuat class dan member lingkaran

--- Pointer.cpp
+++ Pointer.cpp
@@ -20,4 +20,24 @@ public:
     {
         return a * b;
     }
+
+    Lingkaran ol;
+
+};
+
+class Lingkaran
+{
+public:
+    int jarijari;
+
+    void input()
+    {
+        cout << "Masukkan jari-jari :";
+        cin >> jarijari;
+    }
+
+    int luasLingkaran(int r)
+    {
+        return 3.14 * r * r;
+    }
 }
