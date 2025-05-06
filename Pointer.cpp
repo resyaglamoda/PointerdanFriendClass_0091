63333f72c38e1fc758bf856855775af7f5b7d7e6
resyaglamoda <resya glamoda suhendri>
2025-05-06 14:51:12 +0700

    Membuat member class

--- Pointer.cpp
+++ Pointer.cpp
@@ -5,4 +5,19 @@ class PersegiPanjang
 {
 public: // akses modifier
     int panjang, lebar;
+
+public:
+
+    void input()
+    {
+        cout << "masukkan panjang : ";
+        cin >> panjang;
+        cout << "masukkan lebar :";
+        cin >> lebar;
+    }
+
+    int luasPp(int a, int b)
+    {
+        return a * b;
+    }
 }
